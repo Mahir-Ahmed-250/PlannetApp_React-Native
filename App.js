b2e87e9d12ef570c6426a39cd3dccce3955ee79f
unknown <xahin.mahir@gmail.com>
2022-05-28 02:27:36 +0600

    Design Pattern Created

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './src/theme/colors'
+import { spacing } from './src/theme/spacing';
+import { useFonts } from "expo-font";
+import { typography } from './src/theme/typography';
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Antonio-Medium": require('./assets/fonts/Antonio-Medium.ttf'),
+    "Spartan-Bold": require('./assets/fonts/Spartan-Bold.ttf'),
+    "Spartan-Regular": require('./assets/fonts/Spartan-Regular.ttf')
+  })
+
+  if (!loaded) {
+    return (
+      <Text>Font is Loading.........</Text>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: typography.primaryBold, fontSize: spacing[5] }}>Open up App.js to start working on your app!</Text>
+      <Text style={{ marginTop: spacing[4] }}>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +29,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
